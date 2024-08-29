2ff81d6d46a065676db55700d56f1586b1216f42
Axki13029 <axekil2@utb.karlskoga.se>
2024-08-29 08:58:15 +0200

    uträkning

---
+++ Ekv.py
@@ -0,0 +1,11 @@
+num1=float(input("Enter your first number"))
+num2=float(input("enter your second number"))
+R=input("vill du använda mult, div, min eller plus")
+if R==str("mult"):
+    print(num1*num2)
+if R==str("div"):
+    print(num1/num2)
+if R==str("plus"):
+    print(num1+num2)
+if R==str("min"):
+    print(num1-num2)
