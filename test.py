fd480bd84d93f9607d55d7379319622118ea9c2e
Axki13029 <axekil2@utb.karlskoga.se>
2024-09-05 08:25:25 +0200

    Strängar test

---
+++ test.py
@@ -0,0 +1,12 @@
+namn=" axel "
+Namn="hej hej"
+print(namn[::-1])
+print(namn.center(20))
+print(namn.count("i"))
+print(namn.count("ll"))
+print(namn.rjust(20))
+print(namn.ljust(20))
+print(namn.replace(" ","Ö"))
+print(Namn.capitalize())
+print(Namn.title())
+print(namn.strip())
