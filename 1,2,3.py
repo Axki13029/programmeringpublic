7eef37b74822f41b2ae77d78dc89a1e8419a0b20
Axki13029 <axekil2@utb.karlskoga.se>
2024-09-12 08:30:41 +0200

    4

--- 1,2,3.py
+++ 1,2,3.py
@@ -4,7 +4,6 @@ num=int(input("hur många gånger vill du kasta"))
 summa=int(0)
 for i in range (1,num+1):
     kast=random.randint(1,6)
-    print("tärning nummer",i,"blev", kast)
     summa+=kast
 print("summan av tärningar blev",summa)
 print("medelvärdet av tärningar blev",summa/num)
