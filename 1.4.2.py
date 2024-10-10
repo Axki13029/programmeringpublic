4c57b9f9a57497354f59893f8925cbda77328759
Axki13029 <axekil2@utb.karlskoga.se>
2024-10-10 13:03:52 +0200

    s

--- 1.4.2.py
+++ 1.4.2.py
@@ -1,11 +1,9 @@
-#Gör ett program som har följande meny:
-#[L]ägg till temperaturmätning
-#[S]kriv ut alla temperaturer och medeltemperatur
-#[T]a bort temperaturmätning
-#[A]vsluta
-
-#Låt användaren får mata in temperaturmätningar ifrån en väderstation under valet “Lägg till temperaturmätning”. 
-#Varje temperaturmätning skall sparas i en lista i programmet. 
-#När användaren väljer valet “Skriv ut alla temperaturer och medeltemperatur” skall hela listan skrivas ut samt medeltemperaturen. 
-#Användaren skall även under valet “Ta bort temperaturmätning” kunna ta bort en temperaturmätning ifall den vill.
-#Programmet skall inte avslutas förens användaren anger “Avsluta” i menyn.
+#Låt användaren får mata in temperaturmätningar på en väderstation. 
+#Dessa mätningar skall sparas i en lista och användaren skall få skriva så många som den vill.
+#När programmet är färdigt skall listan med alla mätningar skrivas ut och även medeltemperaturen.
+lista=[]
+x=0
+for i in range(1,6):
+    nummer=int(input("skriv ett tal"))
+    lista.append(nummer)
+    x+=nummer
