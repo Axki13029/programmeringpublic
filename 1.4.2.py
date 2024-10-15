ff160a189a5c92face0298664f75bdd7a0c7f507
Axki13029 <axekil2@utb.karlskoga.se>
2024-10-15 09:17:25 +0200

    d

--- 1.4.2.py
+++ 1.4.2.py
@@ -2,8 +2,11 @@
 #Dessa mätningar skall sparas i en lista och användaren skall få skriva så många som den vill.
 #När programmet är färdigt skall listan med alla mätningar skrivas ut och även medeltemperaturen.
 lista=[]
-x=0
-for i in range(1,6):
-    nummer=int(input("skriv ett tal"))
-    lista.append(nummer)
-    x+=nummer
+x=int(input("Skriv hur många temperaturer vill du mata in"))
+for i in range(0,x):
+    temperatur=float(input("Mata in en temperatur"))
+    lista.append(temperatur)
+print(lista)
+G=int(len(lista))
+U=sum(lista)/G
+print(U)
