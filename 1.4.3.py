2cb279bbe53f9100b266d2c579038d9bd2906397
Axki13029 <axekil2@utb.karlskoga.se>
2024-10-17 09:49:42 +0200

    s

--- 1.4.3.py
+++ 1.4.3.py
@@ -4,13 +4,19 @@
 from collections import Counter
 lista=[]
 for i in range(0, 5):
-    men = input("Skriv en mening: ")
-    countmen = men.split(" ")  
-    lista.append(countmen)  
+    mening = input("Skriv en mening: ")
+    count = mening.split(" ")  
+    lista.append(count)
 
 print("osorterad", lista)
 lista.sort(key=len)
 print("sorterad", lista)
 print("din längsta mening är", len(lista[4]), "ord lång")
-print("Medel längden på dina meningarär", (len(lista[0])+len(lista[1])+len(lista[2])+len(lista[3])+len(lista[4])/5))
+o=len(lista[0])
+p=len(lista[1])
+q=len(lista[2])
+t=len(lista[3])
+v=len(lista[4])
+medelvärde=(o+p+q+t+v)/5
+print("medelvärdet av dina meningar är", medelvärde)
  #använde stack overflow för att lära mig "key" och Counter
