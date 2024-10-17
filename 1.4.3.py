dfe0fa1cf2264d72d6beb28945666efbf4e20d85
Axki13029 <axekil2@utb.karlskoga.se>
2024-10-17 08:06:18 +0200

    d

---
+++ 1.4.3.py
@@ -0,0 +1,4 @@
+#Låt användaren skriva in fem meningar.
+#Programmet skall sedan räkna ut medellängden på meningarna. 
+# Programmet skall sedan skriva ut i konsolen hur lång den längsta meningen är
+for i 
